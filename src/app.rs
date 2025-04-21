fed6d3073227e136245070c653555e293c911d6d
Yix0n <wiktor.slodkowski.2004@gmail.com>
2025-04-21 18:05:07 +0200

    Fixed bug that caused Panic on minimalize

--- src/app.rs
+++ src/app.rs
@@ -60,6 +60,10 @@ impl<'a> State<'a> {
     }
 
     pub fn resize(&mut self, new_size: PhysicalSize<u32>) {
+        if new_size.width == 0 || new_size.height == 0 {
+            return;
+        }
+        
         self.config.width = new_size.width;
         self.config.height = new_size.height;
         self.surface.configure(&self.renderer.device, &self.config);
